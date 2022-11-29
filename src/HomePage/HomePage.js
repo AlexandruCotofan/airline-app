fda49b12bf9fd15683fa8f65551c5499b3316468
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-29 09:50:11 +0100

    Create home page

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div id="home-page">
+      <div id="home-page-title">
+        <h1>Airline App</h1>
+        <p>Truly, one of the apps of all time.</p>
+      </div>
+      <button id="search-btn">Search for flights</button>
+    </div>
+  );
+}
+
+export default HomePage;
